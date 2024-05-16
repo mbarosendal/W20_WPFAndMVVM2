64a80a09f3ca04c95dc5a01a8ea8f58043d834d8
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2024-05-16 14:21:58 +0200

    Add project files.

---
+++ WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private PersonRepository PersonRepository;
+
+        public ObservableCollection<PersonViewModel> PersonsVM { get; set; }
+
+        private PersonViewModel selectedPerson;
+
+        public PersonViewModel SelectedPerson
+        {
+            get { return selectedPerson; }
+            set 
+            { 
+                selectedPerson = value; 
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
+
+        public void AddDefaultPerson()
+        {
+            try
+            {
+                // Add the default person to the repository
+                Person defaultPerson = PersonRepository.Add("Specify FirstName", "Specify LastName", 0, "Specify Phone");
+
+                // Create a corresponding view model for the default person
+                PersonViewModel defaultPersonViewModel = new PersonViewModel(defaultPerson);
+
+                // Add the view model to the collection
+                PersonsVM.Add(defaultPersonViewModel);
+
+                // Set the default person as the selected person in the ListBox
+                SelectedPerson = defaultPersonViewModel;
+            }
+            catch (ArgumentException ex)
+            {
+                // Handle exception if arguments are not valid
+                // For example: Log the error, show a message to the user, etc.
+            }
+        }
+        public void DeleteSelectedPerson()
+        {
+            if (SelectedPerson != null)
+            {
+                // Slet den valgte person fra personRepo via PersonViewModel's DeletePerson metode
+                SelectedPerson.DeletePerson(PersonRepository);
+
+                // Fjern den valgte person fra PersonsVM i MainViewModel
+                PersonsVM.Remove(SelectedPerson);
+
+                // Nulstil valgt person til null, så den ikke længere er valgt i ListBox'en
+                SelectedPerson = null;
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            PersonRepository = new PersonRepository();
+            PersonsVM = new ObservableCollection<PersonViewModel>();
+
+            foreach (var person in PersonRepository.GetAll())
+            {
+                PersonsVM.Add(new PersonViewModel(person));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+    }
+}
+
+
---
+++ WPFAndMVVM2/ViewModels/PersonViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+
+        // Properties tilgængelige for View-laget?
+        public string FirstName
+        {
+            get { return person.FirstName; }
+            set
+            {
+                person.FirstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+        public void DeletePerson(PersonRepository personRepo)
+        {
+            if (person != null)
+            {
+                personRepo.Remove(person.Id);
+            }
+        }
+
+        public string LastName
+        {
+            get { return person.LastName; }
+            set
+            {
+                person.LastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public int Age
+        {
+            get { return person.Age; }
+            set
+            {
+                person.Age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public string Phone
+        {
+            get { return person.Phone; }
+            set
+            {
+                person.Phone = value;
+                OnPropertyChanged(nameof(Phone));
+            }
+        }
+
+        // Constructor til initialisering af klassens medlemmer
+        public PersonViewModel(Person person)
+        {
+            this.person = person;
+        }
+
+        // Implementering af INotifyPropertyChanged for at notificere UI om ændringer
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyname)           
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));  
+            }
+        }
+
+    }
+}
